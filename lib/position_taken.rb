a6572c46598ebf5d21f31a0548312f180afab987
Miles Higbie <miles.higbie@gmail.com>
2020-01-02 20:37:35 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -3,7 +3,7 @@
 board = ["X", " ", " ", " ", " ", " ", " ", " ", "O"]
 
 def position_taken?(board,index)
-    if board[index] == " " || "" || nil
+    if board[index] == " " || board[index] == "" 
        return false 
     elsif board[index] == "X" || board[index] == "O"
        return true 
