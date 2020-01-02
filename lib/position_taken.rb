3dba112867ac47a412cb17ac02ee522f04536989
Miles Higbie <miles.higbie@gmail.com>
2020-01-02 20:14:10 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -4,6 +4,6 @@ def position_taken?(board,index)
     if board[index] == " " || ""
       return false 
     elsif board[index] == "X" || "O"
-        return true 
+      return true 
     end 
 end 
