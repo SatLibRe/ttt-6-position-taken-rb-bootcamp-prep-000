1f2abae2ced566ee62fadfdc58e5c8e7ddf63c6e
Miles Higbie <miles.higbie@gmail.com>
2020-01-02 20:20:22 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,5 +1,7 @@
 # code your #position_taken? method here!
 
+board = ["X", " ", " ", " ", " ", " ", " ", " ", "O"]
+
 def position_taken?(board,index)
     if board[index] == " " || ""
       return false 
